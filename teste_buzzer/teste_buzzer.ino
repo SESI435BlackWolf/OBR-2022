01cb21508337f664e560f88db05a5053faed668e
Guilherme Souza <guilherme.meneghinsouza@gmail.com>
2022-06-10 01:08:44 -0300

    Corrigindo a implementação dos testes para o buzzer.

--- teste_buzzer/teste_buzzer.ino
+++ teste_buzzer/teste_buzzer.ino
@@ -1,35 +1,35 @@
-byte buzzer_pin = 11;
+byte buzzer_pino = 11;
 
 void tocar (bool musica) {
     if (musica) {
         for (byte x = 0; x < 6; x ++) {
-            tone(buzzer_pino, 500);
-            delay(250);
-            tone(buzzer_pin, 5000);
-            delay(250);
+            tone(buzzer_pino, 500, 250);
+            delay(300);
+            tone(buzzer_pino, 4500, 250);
+            delay(300);
         }
     }
     else {
         for (byte x = 0; x < 3; x ++) {
-            tone(buzzer_pin, 700);
-            delay(300);
-            tone(buzzer_pin, 5000);
-            delay(300);
+            tone(buzzer_pino, 500, 300);
+            delay(350);
+            tone(buzzer_pino, 4500, 300);
+            delay(350);
         }
-    digitalWrite(buzzer_pin, LOW);
+        noTone(buzzer_pino);
     }
 }
 
 void setup() {
     // put your setup code here, to run once:
-    pinMode(buzzer_pin, OUTPUT);
-    
+    pinMode(buzzer_pino, OUTPUT);
+
 }
 
 void loop() {
     // put your main code here, to run repeatedly:
     tocar(0);
-    delay(1500);
+    delay(1000);
     tocar(1);
-    delay(1500);
+    delay(1000);
 }
