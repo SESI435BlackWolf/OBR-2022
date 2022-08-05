a115b03a80ddb1e5abed21dc9772bcd80460db5b
Guilherme Souza <guilherme.meneghinsouza@gmail.com>
2022-08-05 20:16:59 -0300

    Implementação dos testes dos sensores reflexivos.

---
+++ Testes/sensores_reflexivos/sensores_reflexivos.ino
@@ -0,0 +1,31 @@
+class Sensor {
+    public:
+        byte pinoEntrada;
+
+        Sensor(byte pino) {
+            this->pinoEntrada = pino;
+        }
+
+        byte ler() {
+            return (analogRead(pinoEntrada));
+        }
+};
+
+
+Sensor sensorExEsquerda(A0);    // Amarelo
+Sensor sensorEsquerda  (A1);    // Azul
+Sensor sensorDireita   (A2);    // Roxo
+Sensor sensorExDireita (A1);    // Verde
+
+void setup() {
+    Serial.begin(9600);
+
+}
+
+void loop() {
+    Serial.print("ExD: "    ); Serial.print(sensorExDireita.ler());
+    Serial.print("\t| D: "  ); Serial.print(sensorDireita.ler());
+    Serial.print("\t| E: "  ); Serial.print(sensorEsquerda.ler());
+    Serial.print("\t| ExE: "); Serial.print(sensorExEsquerda.ler());
+    Serial.println();
+}
